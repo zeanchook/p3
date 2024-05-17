3d30b613e5d3b258459d5baf72912101e4c324c0
MollyMai99 <miaoling.mai99@gmail.com>
2024-05-17 10:45:22 +0800

    feat:HomePage add brand info and banner image

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,28 @@
+/* eslint-disable react/no-unescaped-entities */
 import PopularItems from "../../components/PopularItems";
 
 export default function HomePage() {
   return (
     <>
-      <div>brand info and service</div>
-      <div>promo baner--picture</div>
+      <img
+        src="https://t3.ftcdn.net/jpg/04/65/46/52/360_F_465465254_1pN9MGrA831idD6zIBL7q8rnZZpUCQTy.jpg"
+        alt="Banner"
+      />
+      <div>
+        <h1>About Us:</h1>
+        <p>
+          Welcome to TrendyHub, your number one source for the latest and
+          greatest in fashion. We are dedicated to providing you the best of
+          apparel, with a focus on quality, customer service, and uniqueness.
+          TrendyHub started in 2022, and has come a long way from its beginnings
+          in a small office. When we first started out, our passion for
+          eco-friendly and stylish clothing drove us to start our own business.
+          We hope you enjoy our products as much as we enjoy offering them to
+          you. If you have any questions or comments, please do not hesitate to
+          contact us.
+        </p>
+      </div>
+      <hr />
       <PopularItems />
     </>
   );
