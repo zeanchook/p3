3d1a7e01a021c7f8792384259c2181f018f12e53
zeanchook50 <zhiyanchook@gmail.com>
2024-05-20 20:51:39 +0800

    feat:add route for update order status

--- routes/api/ordersRouter.js
+++ routes/api/ordersRouter.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const orderController = require("../../controllers/api/orderController");
 
-router.post("/update", orderController.updateOrder);
-router.post("/update/:orderId/", orderController.createOrderLine);
+router.post("/update/:orderId/", orderController.updateOrder);
+router.post("/create/:orderId/", orderController.createOrderLine);
 router.delete("/update/:orderId/", orderController.deleteOrder);
 
-router.get("/order/:orderId", orderController.getOrder);
-router.get("/getuseOrder/:userId", orderController.getUserOrders);
+router.get("/order", orderController.getOrder); //orders
+router.post("/order/updateStatus", orderController.updateOrderStatus);
+router.get("/getuseOrder/:userId", orderController.getUserOrders); //
 router.get("/:userId/orders", orderController.getUserOrdersById);
 router.get("/checkout/:orderId", orderController.getUserByOrderId);
 router.patch("/:orderId/:userId/paid", orderController.updateOrderPaid);
