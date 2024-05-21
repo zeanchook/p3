9c5b2630c38c962d18bb740c24808f27d7345d13
zeanchook50 <zhiyanchook@gmail.com>
2024-05-21 20:11:37 +0800

    feat: add base:'/show/' to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/show/",
   server: {
     proxy: {
       "/api": "http://localhost:3000",
