2cb880993c10a44abdd9a617ce35dd2db7b60310
zeanchook50 <zhiyanchook@gmail.com>
2024-05-21 20:47:19 +0800

    redeploy for test

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const path = require("path");
-// const favicon = require("serve-favicon");
 const logger = require("morgan");
 const debug = require("debug")("mern:server");
-// Always require and configure near the top
 require("dotenv").config();
 require("./config/database");
 
@@ -11,10 +9,8 @@ const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use("/public", express.static("public"));
 
-// Configure both serve-favicon & static middleware
-// to serve from the production 'build' folder
-// app.use(favicon(path.join(__dirname, "dist", "vite.svg")));
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(require("./config/checkToken").checkTokenMiddleware);
 // Put API routes here, before the "catch all" route
