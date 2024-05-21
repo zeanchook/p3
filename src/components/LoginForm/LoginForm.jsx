91c2f3a9a6215ba91cb150edab001b0af24ccdac
zeanchook50 <zhiyanchook@gmail.com>
2024-05-21 19:42:43 +0800

    change file path

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -75,7 +75,7 @@ export default function LoginForm({ setUser,setLoginState }) {
     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
       <img
         className="mx-auto h-16 w-auto"
-        src="../../../public/images.png"
+        src="/images.png"
         alt="Your Company"
       />
       <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
