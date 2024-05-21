3abecf5c2fef980b7506b760591f708449fb6a5a
zeanchook50 <zhiyanchook@gmail.com>
2024-05-21 15:18:43 +0800

    feat:remove comments

--- atom.js
+++ atom.js
@@ -1,12 +1,10 @@
 import { atom } from "jotai";
 import { getUser } from "./src/utilities/users-service";
-import sendRequest from "./src/utilities/send-request";
 
 const cartItems = atom("");
 const loginSts = atom(getUser());
+console.log(loginSts);
 
-const test = sendRequest(`/api/orders/getUserOrders2/`);
-console.log("here", test);
 const adminUserOrder = atom("");
 
 export { cartItems, loginSts, adminUserOrder };
