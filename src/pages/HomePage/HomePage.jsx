184e45935fc6f448720b7bc89a6241dc318cc090
WAILENGL <162903708+WAILENGL@users.noreply.github.com>
2024-05-22 20:17:16 +0800

    Feat: Added new text and banner to home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,48 +1,58 @@
 /* eslint-disable react/no-unescaped-entities */
-import PopularItems from "../../components/PopularItems";
-import { useEffect } from "react";
-import { useAtom } from "jotai";
-import { cartItems } from "../../../atom";
-import sendRequest from "../../utilities/send-request";
+import PopularItems from '../../components/PopularItems';
+import { useEffect } from 'react';
+import { useAtom } from 'jotai';
+import { cartItems } from '../../../atom';
+import sendRequest from '../../utilities/send-request';
 
 export default function HomePage() {
-  const [cartState, setCartState] = useAtom(cartItems);
+	const [cartState, setCartState] = useAtom(cartItems);
 
-  console.log(cartState);
-  useEffect(() => {
-    async function getDetails() {
-      const cartItems = await sendRequest(`/api/orders/getUserOrders/`);
-      setCartState(cartItems);
-    }
-    getDetails();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+	console.log(cartState);
+	useEffect(() => {
+		async function getDetails() {
+			const cartItems = await sendRequest(`/api/orders/getUserOrders/`);
+			setCartState(cartItems);
+		}
+		getDetails();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  return (
-    <>
-      <img
-        src="https://t3.ftcdn.net/jpg/04/65/46/52/360_F_465465254_1pN9MGrA831idD6zIBL7q8rnZZpUCQTy.jpg"
-        alt="Banner"
-      />
-      <div>
-        <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-          About Us:
-        </h1>
+	return (
+		<>
+			<img
+				src="https://qhq1h9t6kkqlkwsy-13917519929.shopifypreview.com/cdn/shop/files/site-banner_1080x.jpg"
+				alt="Banner"
+			/>
+			<div>
+				<h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl px-20">
+					About Us:
+				</h1>
 
-        <p className="mt-4 text-xl text-gray-500">
-          Welcome to TrendyHub, your number one source for the latest and
-          greatest in fashion. We are dedicated to providing you the best of
-          apparel, with a focus on quality, customer service, and uniqueness.
-          TrendyHub started in 2022, and has come a long way from its beginnings
-          in a small office. When we first started out, our passion for
-          eco-friendly and stylish clothing drove us to start our own business.
-          We hope you enjoy our products as much as we enjoy offering them to
-          you. If you have any questions or comments, please do not hesitate to
-          contact us.
-        </p>
-      </div>
-      <hr />
-      <PopularItems />
-    </>
-  );
+				<p className="mt-4 text-xl text-gray-500 px-20">
+					Welcome to 3D Printed Wonders, your gateway to a world of creativity
+					and innovation! Dive into a realm where imagination knows no bounds
+					and craftsmanship meets affordability. At 3D Printed Wonders, we
+					curate a collection of awe-inspiring creations crafted by renowned 3D
+					Printing designers, bringing you the marvels of 3D printing at
+					accessible prices.
+				</p>
+
+				<p className="mt-4 text-xl text-gray-500 px-20">
+					Explore our catalog and discover a fusion of artistry and technology,
+					where each piece tells a story and every design captivates the senses.
+					Join us on a journey where the extraordinary becomes ordinary, and
+					where the wonders of 3D printing are just a click away.
+				</p>
+				<p className="mt-4 text-xl text-gray-500 px-20">
+					Experience the future of craftsmanship with 3D Printed Wonders – where
+					innovation meets affordability, and imagination knows no limits.
+				</p>
+			</div>
+			<hr />
+			<div className="mt-10 text-center">
+				<PopularItems />
+			</div>
+		</>
+	);
 }
