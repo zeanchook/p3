2afa891ec39ac8145522d351b1535076d2df85fd
zeanchook50 <zhiyanchook@gmail.com>
2024-05-22 09:24:51 +0800

    feat:added tailwind for signup form

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -41,84 +41,162 @@ export default class SignUpForm extends Component {
 
   render() {
     return (
-      <><form onSubmit={this.handleSubmit}>
-        <fieldset>
-          <legend>SignUp</legend>
+      <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-16 w-auto"
+          src="/images.png"
+          alt="Your Company"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign up a new account
+        </h2>
+      </div>
+  
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST" onSubmit={this.handleSubmit}>
+        <div>
+            <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+              Name
+            </label>
+            <div className="mt-2">
+              <input
+                id="name"
+                name="name"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
 
-          <label>
-            Name:
-            <input
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
+          <div>
+            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+            Username
+            </label>
+            <div className="mt-2">
+              <input
+                id="username"
+                name="username"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
 
-          <label>
-            Email:
-            <input
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+            Password
+            </label>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
 
-          <label>
-            Address:
-            <input
-              name="address"
-              value={this.state.address}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
+          <div>
+            <label htmlFor="confirm" className="block text-sm font-medium leading-6 text-gray-900">
+            Password Confirmation
+            </label>
+            <div className="mt-2">
+              <input
+                id="confirm"
+                name="confirm"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={this.state.confirm}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
 
-          <label>
-            Username:
-            <input
-              name="username"
-              value={this.state.username}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
+          <div>
+            <label htmlFor="usertype" className="block text-sm font-medium leading-6 text-gray-900">
+            User Type
+            </label>
+            <div className="mt-2">
+              <input
+                id="usertype"
+                name="usertype"
+                placeholder=" Input token for admin"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={this.state.usertype}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
 
-          <label>
-            Password:
-            <input
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
 
-          <label>
-            UserType:
-            <input
-              name="usertype"
-              value={this.state.usertype}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-
-          <label>
-            Confirm:
-            <input
-              name="confirm"
-              value={this.state.confirm}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-
-          <button>Sign Up</button>
-          <p>{this.state.error} </p>
-        </fieldset>
-      </form>Or already have an account<button onClick={()=> this.props.setLoginState("login")}>Login</button></>
-    );
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                // autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+  
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="address" className="block text-sm font-medium leading-6 text-gray-900">
+              Address
+              </label>
+              <div className="text-sm">
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="address"
+                name="address"
+                type="address"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={this.state.address}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+  
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Create Account
+            </button>
+          </div>
+        </form>
+  
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Already have an account?{' '}
+          <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500" onClick={()=> this.props.setLoginState("login")}>
+            Sign in
+          </a>
+        </p>
+      </div>
+    </div>
+  </>
+    
+      );
   }
 }
